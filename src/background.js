835217e8fded7b8f0a21cdc39300d71daaac7ac4
Anton Molganov <anton.molganov@gmail.com>
2018-01-26 14:31:46 +0500

    Add files via upload

---
+++ src/background.js
@@ -0,0 +1,95 @@
+console.log("background.js injected");
+chrome.browserAction.onClicked.addListener(function(tab) {
+   chrome.tabs.sendMessage(tab.id, {source:"background", type:"click", message:"to front"}, function(result){});
+});
+
+var songs = [];
+var working = false;
+
+chrome.extension.onMessage.addListener(function(request, sender, callback){
+   if (request.source == "front") {
+      console.log("message recevied: " + "type:"+request.type);
+      if (request.type == "notice"){
+            console.log("message: " + request.message);
+      }else if (request.type == "download"){
+            if (working) {
+                  console.log("Already in download process, skipping command.");
+            }else{
+                  working = true;            
+                  console.log("Download process started.");
+            };
+            songs = request.message;
+            startNextDownload();
+//            for (var i = 0; i < songs.length; i++){
+//                  var url = songs[i].url;
+//                  var filename = replaceForbidden(decodeHtml(    songs[currentDownloadIndex].name.trim() + " - " + songs[currentDownloadIndex].title.trim() + ".mp3"    ));
+//                  console.log(filename);
+//                  console.log(filename);
+//              }
+      }
+   }
+});
+
+var currentDownloadIndex = 0;
+var DOWNLOAD_CHECK_INTERVAL = 500;
+
+function startNextDownload(){
+   if (currentDownloadIndex >= songs.length) {
+         working = false;
+         return;
+   };
+   var checker = function(id){
+         if (id){
+            var wait = function(){
+                  chrome.downloads.search({"id":id}, function(foundItems){
+                     if (foundItems && foundItems.length > 0){
+                           var state = foundItems[0].state;
+                           if (state == "in_progress"){
+                                 setTimeout(wait, DOWNLOAD_CHECK_INTERVAL)
+                           }else if (state == "interrupted"){
+                                 startNextDownload(); 
+                           }else{
+                                 currentDownloadIndex++;                           
+                                 startNextDownload(); 
+                           };
+                     }else{
+                           startNextDownload();
+                     };
+                  });                  
+
+            };
+            wait();
+         }else{
+            startNextDownload();
+         };
+   };
+   startDownload(currentDownloadIndex, checker);
+}
+
+function startDownload(id, callBack){
+   var url = songs[currentDownloadIndex].url;
+   var filename = replaceForbidden(decodeHtml(    songs[currentDownloadIndex].name.trim() + " - " + songs[currentDownloadIndex].title.trim() + ".mp3"    ));
+   chrome.downloads.download({url:      url,
+                              filename: filename,
+                              conflictAction: "overwrite"}, function(id){
+                                                               callBack(id);
+                                                            });
+};
+
+function normalizeString(str) {
+   return str.replace(/&#([0-9]{1,4});/gi, function(match, numStr) {
+      var num = parseInt(numStr, 10);
+      return String.fromCharCode(num);
+   });
+}
+
+function decodeHtml(html) {
+   var txt = document.createElement("textarea");
+   txt.innerHTML = html;
+   return txt.value;
+}
+
+function replaceForbidden(str){
+   return str.replace(/[\\/:"*?<>|]/g, " ");
+}
+
